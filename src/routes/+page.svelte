cb07206221969020cbf8eb02e085fb3e257eceb0
david <david.muelle2@web.de>
2024-05-26 16:45:31 +0200

    Local check debug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,9 @@
 	import { nodes } from '../../.svelte-kit/generated/client/app';
 	import BaseDevice from '$lib/api/devices/BaseDevice';
 	import { connectedStore, HubConnectionStatus, socketStore } from '$lib/store/GeneralStore';
-	import { isLocal } from '$lib/util/EnvChecker';
 
-	console.log('isLocal:', isLocal);
+	console.log("HOST");
+	console.log(window.location.host);
 
 	const modalStore = getModalStore();
 
