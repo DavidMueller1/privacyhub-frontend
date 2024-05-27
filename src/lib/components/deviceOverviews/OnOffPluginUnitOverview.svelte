ed1021c30dfed02964fadc648ab8b9e8a8abca69
david <david.muelle2@web.de>
2024-05-27 17:42:52 +0200

    Debug

--- src/lib/components/deviceOverviews/OnOffPluginUnitOverview.svelte
+++ src/lib/components/deviceOverviews/OnOffPluginUnitOverview.svelte
@@ -26,6 +26,7 @@
 
 	// UI events
 	const onOffStateChanged = () => {
+		console.log('======device.state', device.state);
 		ApiClient.setOnOff(accessLevel, device.nodeId, device.endpointId, device.state)
 			.then(() => {
 				device.state = !device.state;
