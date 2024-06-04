b66fc1eecb61cee74c6550a7fbe090e206c5fd1c
DavidMueller1 <david.muelle2@web.de>
2024-06-05 00:53:44 +0200

    Filled about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,13 +2,29 @@
 import StateGraphic from '$lib/components/util/StateGraphic.svelte';
 </script>
 
-<div class="container h-full mx-auto flex flex-col items-center">
+<div class="container h-full mx-auto flex flex-col self-center items-center max-w-screen-xl px-4 xl:px-8">
 	<h2 class="h2 mt-4">About</h2>
-	<div class="flex flex-col items-center xl:flex-row xl:items-start mt-2 space-x-4 px-8">
+	<div class="mb-2 xl:mb-0">
+		<h3 class="h3 mt-4 mb-2">Dashboard</h3>
+		Übersicht über verbundene Geräte, deren Status und deren Privacy State
+		Bei Klick auf ein Gerät: Weitere Details sowie Kontrolle der Privacy States + Proxy Einstellungen
+		Hinzufügen von weiteren Geräten (nur lokal)
+		<h3 class="h3 mt-4 mb-2">History</h3>
+		Übersicht über vergangenen Status der Geräte und deren Privacy State
+		Das Gerät kann durch das Klicken auf den Gerätenamen ausgewählt werden
+		Zeitraum kann angepasst werden durch
+		das Klicken auf einen der Standart-Zeitraum-Buttons
+		das Bearbeiten des Start- oder Endzeitpunkts
+		ziehen oder zoomen im Graphen selbst
+	</div>
+	<div class="flex flex-col items-center xl:flex-row xl:items-start my-4 xl:space-x-4">
 <!--		<div class="bg-red-500 shrink-0 grow-0 h-32" style="width: 500px"></div>-->
-		<StateGraphic classString="w-auto sm:w-[500px] shrink-0 grow-0" width={500} />
-		<div class="flex-grow-1 mt-4 xl:mt-0">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+		<StateGraphic classString="w-auto sm:w-[400px] shrink-0 grow-0" width={400} />
+		<div class="flex-grow-1 mt-4 space-y-4 xl:mt-0">
+			<h3 class="h3 mb-2">States</h3>
+			<p>In <span class="text-state-local font-bold text-xl">Local mode</span> all devices are only visible from within the home network. While this poses the least security and privacy vulnerabilities, it also offers the most limited feature set.</p>
+			<p>The <span class="text-state-online font-bold text-xl">Online mode</span> encompasses the Local mode but additionally grants the user access to the system from anywhere in the world using a dedicated account for encryption. On the Online-Website, the device-history only includes the time where the devices were in Online mode or higher. As data leaves the home network, the threat to privacy and security is increased.</p>
+			<p>The <span class="text-state-online-shared font-bold text-xl">Online-Shared mode</span> includes the features from both previous modes, but also gives the user the ability to connect third party devices like e.G. Alexa or Google Home. This mode offers the most features, but also has the highest vulnerabilities to privacy and security threats, as the Privacy-Hub Eco-System no longer has complete control over what happens to your data when it can be accessed by third party devices.</p>
 		</div>
 	</div>
 </div>
